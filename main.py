d07a137500928f5d9e9148231d15745dd183a8da
Luca Di Felice <Luca.difelice@hts.on.ca>
2023-03-09 10:08:49 -0500

    more thinking

--- main.py
+++ main.py
@@ -6,17 +6,24 @@ def main():
 
     counter = 0
 
+    Xn = []
+
+    An = []
+
     dergee = input( "What is the degree of the polynomial:\n" )
 
-    polynomial = input( "Enter in the polynomial numbers (add spaces):\n" )
+    polynomial = input( "Enter in the polynomial (add spaces):\n" )
 
+    Xn = polynomial.replace("", "",)
 
 def LongDivision( divisor, divident, quotient, remainder ):
     print( "" )
 
 
-def FactorTheorem():
-    print( "" )
+def FactorTheorem( polynomial ):
+    
+
+
 
 
 
