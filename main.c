553bd616080fbef46f2a2365747b781cfd6de175
Luca Di Felice <Luca.difelice@hts.on.ca>
2023-03-14 20:48:04 -0400

    I made the user prompt :)

--- main.c
+++ main.c
@@ -3,9 +3,39 @@
 
 int main() {
 
-    
+    int Counter = 0;
 
-    printf( "Please enter in a polynomial:\n" );
+    const int MaxCharacters = 32;
+
+    int Variables[MaxCharacters];
+
+    int Coeficints[MaxCharacters];
+
+    int NumOfCoefficient = 0;
+
+    printf( "Enter in the Num of values inside the polynomial:\n" );
+    scanf( "%d", &NumOfCoefficient );
+    fgetc( stdin );
+
+    while ( Counter < NumOfCoefficient ) {
+
+        printf( "Enter in coefficient number #%d: ", NumOfCoefficient - Counter );
+        scanf( "%d", &Coeficints[Counter] );
+        fgetc( stdin );
+
+        printf( "Enter in the variable value for that number: " );
+        scanf( "%d", &Variables[Counter] );
+        fgetc( stdin );
+
+        Counter++;
+
+    }
+
+}
+
+int checkFactors() {
+
+    
 
 }
 
